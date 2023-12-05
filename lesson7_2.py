6de1a7009279cededfa78bfa8f86b539fad4190e
robinchen02 <151026829+robinchen02@users.noreply.github.com>
2023-12-05 13:28:02 +0000

    modify

--- lesson7_2.py
+++ lesson7_2.py
@@ -1,25 +1,30 @@
 import pyinputplus as pyip
 import random
-count=0
-min=1
-max=100
-random_number=random.randint(min,max)
-print("===========猜數字遊戲=============")
 while(True):
-    key_in=pyip.inputInt(f"猜數字範圍{min}~{max}")
-    count+=1
-    print(key_in)
-    if key_in==random_number:
-        print(f"Bingo!猜對了,答案是{random_number}")
-        print(f"您猜了{count}次")
+    count=0
+    min=1
+    max=100
+    random_number=random.randint(min,max)
+    print(random_number)
+    print("===========猜數字遊戲=============")
+    while(True):
+        key_in=pyip.inputInt(f"猜數字範圍{min}~{max}")
+        count+=1
+        print(key_in)
+        if key_in==random_number:
+            print(f"Bingo!猜對了,答案是{random_number}")
+            print(f"您猜了{count}次")
+            break
+        elif key_in > random_number:
+            max=key_in-1
+            print(f"猜錯了，再小一點")
+            print(f"您猜了{count}次")
+        elif key_in < random_number:
+            min=key_in+1
+            print(f"猜錯了，再大一點")
+            print(f"您猜了{count}次")
+        print("=================================")
+    is_continue=pyip.inputYesNo("請問還要繼續嗎?(y/n)")
+    if is_continue == "no":
         break
-    elif key_in > random_number:
-        max=key_in-1
-        print(f"猜錯了，再小一點")
-        print(f"您猜了{count}次")
-    elif key_in < random_number:
-        min=key_in+1
-        print(f"猜錯了，再大一點")
-        print(f"您猜了{count}次")
-    print("=================================")
 print("遊戲結束")

b64ca927a4c8659fac99cebfa8e414965073743e
robinchen02 <151026829+robinchen02@users.noreply.github.com>
2023-12-14 14:02:30 +0000

    modify

--- lesoon10_3.py
+++ lesoon10_3.py
@@ -2,15 +2,17 @@ import random
 import pyinputplus as pyip
 
 
-def getStudents() -> list[list]:
+def getStudents(student_nums:int=1,scores_nums:int=2) -> list[list]:
+    '''
+    參數: student_nums -> 學生人數\n
+    參數: scores_nums -> 科目數\n
+    '''
+
+
     with open("names.txt",mode="r",encoding="utf-8") as file:
         names:str = file.read()
 
     nameslist:list[str] = names.split("\n")
-
-    student_nums:int = pyip.inputInt("請輸入學生的人數(1~50):",min=1,max=50)
-    scores_nums:int = pyip.inputInt("請輸入科目數(1~7)",min=1,max=7)
-
     students:list[list] = []
 
 
@@ -23,5 +25,12 @@ def getStudents() -> list[list]:
         students.append(stu)
 
     return(students)
-students:list[list] = getStudents()
-print(students)
+#students:list[list] = getStudents()
+#print(students)
+
+if __name__ == '__main__':
+    students:list[list] = getStudents()
+    s_nums:int = pyip.inputInt("請輸入學生的人數(1~50):",min=1,max=50)
+    o_nums:int = pyip.inputInt("請輸入科目數(1~7):",min=1,max=7)
+    students:list[list] = getStudents(student_nums=s_nums,scores_nums=o_nums)
+    print(students)

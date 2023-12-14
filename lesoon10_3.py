8552340ceca8453b6968ef52c0c2e8ae5f510c6d
robinchen02 <151026829+robinchen02@users.noreply.github.com>
2023-12-14 13:32:15 +0000

    modify

--- lesoon10_3.py
+++ lesoon10_3.py
@@ -2,24 +2,26 @@ import random
 import pyinputplus as pyip
 
 
+def getStudents() -> list[list]:
+    with open("names.txt",mode="r",encoding="utf-8") as file:
+        names:str = file.read()
 
-with open("names.txt",mode="r",encoding="utf-8") as file:
-    names:str = file.read()
+    nameslist:list[str] = names.split("\n")
 
-nameslist:list[str] = names.split("\n")
+    student_nums:int = pyip.inputInt("請輸入學生的人數(1~50):",min=1,max=50)
+    scores_nums:int = pyip.inputInt("請輸入科目數(1~7)",min=1,max=7)
 
-student_nums:int = pyip.inputInt("請輸入學生的人數(1~50):",min=1,max=50)
-scores_nums:int = pyip.inputInt("請輸入科目數(1~7)",min=1,max=7)
-
-students:list[list] = []
+    students:list[list] = []
 
 
-names:list[str] = random.choices(nameslist,k=student_nums)
-for name in names:
-    stu:list[int|str] = []
-    stu.append(name)
-    for i in range(scores_nums):
-        stu.append(random.randint(40,100))
-    students.append(stu)
+    names:list[str] = random.choices(nameslist,k=student_nums)
+    for name in names:
+        stu:list[int|str] = []
+        stu.append(name)
+        for i in range(scores_nums):
+            stu.append(random.randint(40,100))
+        students.append(stu)
 
+    return(students)
+students:list[list] = getStudents()
 print(students)

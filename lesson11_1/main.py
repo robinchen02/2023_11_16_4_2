9b5737aeb71b99715d96af889d4b52033208e7a0
robinchen02 <151026829+robinchen02@users.noreply.github.com>
2023-12-19 12:23:54 +0000

    modify

--- lesson11_1/main.py
+++ lesson11_1/main.py
@@ -29,8 +29,10 @@ def getStudents(student_nums:int=1,scores_nums:int=2) -> list[list]:
 #students:list[list] = getStudents()
 #print(students)
 
-def saveToCSV(fileName:str,data:list[list])-> None:
+def saveToCSV(fileName:str,data:list[list],subject_num:int)-> None:
     fileName+=".csv"
+    subjects=[f'科目{i+1}' for i in range(subject_num)]
+    fields=['姓名'].extend(subjects)
     with open(fileName,mode='w',encoding='utf-8',newline='') as file:
         writer = csv.writer(file)
         #writer.writerow(['姓名',''])
@@ -42,5 +44,5 @@ if __name__ == '__main__':
     o_nums:int = pyip.inputInt("請輸入科目數(1~7):",min=1,max=7)
     students:list[list] = getStudents(student_nums=s_nums,scores_nums=o_nums)
     fileName=pyip.inputFilename("請輸入檔案名稱(不用輸入副檔名)")
-    saveToCSV(fileName=fileName,data=students)
+    saveToCSV(fileName=fileName,data=students,subject_num=o_nums)
     print(students)
